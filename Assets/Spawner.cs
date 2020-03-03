435e3ba0ad7110e2312a3652e2d75c69aaed775f
3189Captain <oogabooga300@gmail.com>
2020-03-02 23:04:16 -0700

    initial commit

---
+++ Assets/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Shoot", 3f, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shoot()
+    {
+        Instantiate(Resources.Load("Bullet"), this.transform);
+    }
+}
